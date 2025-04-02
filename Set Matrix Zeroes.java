d73c73627108ad2a2f49383ea25da52baf50ed95
agrawalvanshika <167639340+agrawalvanshika@users.noreply.github.com>
2025-04-02 11:12:08 +0530

    Create Set Matrix Zeroes.java

---
+++ Set Matrix Zeroes.java
@@ -0,0 +1,43 @@
+public class SetMatrixZeroes {
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        
+        // Mark the rows and columns that need to be set to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        
+        // Set marked rows and columns to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+        
+        setZeroes(matrix);
+        System.out.println("Modified Matrix:");
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
