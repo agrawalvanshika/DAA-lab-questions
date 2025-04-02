a107e23e46538e82faee2927abfb7d702f157468
agrawalvanshika <167639340+agrawalvanshika@users.noreply.github.com>
2025-04-02 10:49:39 +0530

    Create Remove Duplicates from Array.java

---
+++ Remove Duplicates from Array.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        Arrays.sort(arr);
+        int j = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+        for (int i = 0; i <= j; i++) System.out.print(arr[i] + " ");
+    }
+}
