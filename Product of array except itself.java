73f9956c0f1f2e99e67f26cc4928f19faafcadc3
agrawalvanshika <167639340+agrawalvanshika@users.noreply.github.com>
2025-04-02 11:07:34 +0530

    Create Product of array except itself.java

---
+++ Product of array except itself.java
@@ -0,0 +1,22 @@
+public class ProductExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] result = productExceptSelf(nums);
+        for (int num : result) System.out.print(num + " ");
+    }
+}
