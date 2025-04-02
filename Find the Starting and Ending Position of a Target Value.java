f787b5d78fde5ead59268dc0ef590dbf826a54d6
agrawalvanshika <167639340+agrawalvanshika@users.noreply.github.com>
2025-04-02 11:01:05 +0530

    Create Find the Starting and Ending Position of a Target Value.java

---
+++ Find the Starting and Ending Position of a Target Value.java
@@ -0,0 +1,30 @@
+public class SearchRange {
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+    private static int findBound(int[] nums, int target, boolean first) {
+        int left = 0, right = nums.length - 1, bound = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                bound = mid;
+                if (first) right = mid - 1;
+                else left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return bound;
+    }
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] result = searchRange(nums, target);
+        System.out.println("Starting index: " + result[0] + ", Ending index: " + result[1]);
+    }
+}
+
